1a612a8d0459f737a0702d9b29b7afd119235702
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-04-10 13:27:48 +0530

    Add Header and Image in code

---
+++ src/App1.js
@@ -0,0 +1,12 @@
+
+import Header from "./components/Layout/Header.js";
+import React ,{ Fragment } from "react";
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import classes from "./Header.module.css"
+import React,{Fragment} from 'react';
+import HeaderCartButton from "./HeaderCartButton";
+import MealImage from "../../assets/MealImage.jpg"
+const Header= props =>{
+
+    return(
+        <Fragment>
+            <header className={classes.header}>
+                <h1>FoodHub</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={ MealImage} alt='A table full of delicious food'/>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Header;
